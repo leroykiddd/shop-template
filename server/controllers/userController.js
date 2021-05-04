349909df3250d1e145b9c9af920a5e0551ee1ae6
Denis <leroykid02@mail.ru>
2021-05-04 15:41:10 +0300

    Обработка ошибок на сервере

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,3 +1,21 @@
-class User {
-    
+const ApiError = require('../error/ApiError')
+
+class UserController {
+    async registration(req, res) {
+
+    }
+
+    async login(req, res) {
+        
+    }
+
+    async isAuth(req, res, next) {
+        const { id } = req.query
+        if (!id) {
+            return next(ApiError.badRequest('Не указан id пользователя!'))
+        }
+        res.json(id)
+    }
 }
+
+module.exports = new UserController()
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express')
 const router = new Router()
+const userController = require('../controllers/userController')
 
-router.post('/login', )
-router.post('/registration', )
-router.get('/auth', )
+router.post('/login', userController.login)
+router.post('/registration', userController.registration)
+router.get('/auth', userController.isAuth)
 
 module.exports = router
